1e11c88697f2ace0b073b78457633d5af6ccc7a7
Ryan Houdek <Sonicadvance1@gmail.com>
2022-09-07 19:07:41 -0700

    simulator: Add support for indirect runtime call

--- src/aarch64/simulator-aarch64.h
+++ src/aarch64/simulator-aarch64.h
@@ -5142,6 +5142,9 @@ class Simulator : public DecoderVisitor {
   // Simulate a runtime call.
   void DoRuntimeCall(const Instruction* instr);
 
+  // Simulate an indirect runtime call.
+  void DoIndirectRuntimeCall(const Instruction* instr);
+
   // Processor state ---------------------------------------
 
   // Simulated monitors for exclusive access instructions.
